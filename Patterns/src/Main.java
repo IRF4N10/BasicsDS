1d8b2a88bc7208777afd9ec6f3d88aa6d6a62f35
IRF4N10 <irfankhanirfan.official@gmail.com>
2022-12-12 20:38:30 +0600

    Test

--- Patterns/src/Main.java
+++ Patterns/src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner I = new Scanner(System.in);
         Pattern Obj = new Pattern(5);
         Obj.pattern8();
 
